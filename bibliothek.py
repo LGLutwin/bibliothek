b4707048d5b20a81a73b1767fb6b8e577a552e7f
lutwin <lutwingebauer@gmail.com>
2025-05-12 14:38:06 +0200

    add_database

---
+++ bibliothek.py
@@ -0,0 +1,29 @@
+import sqlite3
+conn = sqlite3.connect('bibliothek.db') 
+cursor = conn.cursor()  
+cursor.execute(""" 
+
+    CREATE TABLE bibliothek ( 
+
+        id INTEGER PRIMARY KEY AUTOINCREMENT, 
+
+        titel TEXT NOT NULL, 
+
+        anzahl TEXT NOT Null,
+
+        status TEXT NOT Null,
+
+        standort TEXT ,
+
+    ) 
+
+""") 
+cursor.execute(f"INSERT INTO bibliothek (IDD, titel, author, status, standort) VALUES ('{100010}', '{die_Zahlen}', '{aktiv}', '{toppingprice}', '{typeprice}')") 
+
+         
+
+conn.commit()  
+
+conn.close() 
+
+
